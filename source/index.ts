e97578ca418f21d708ce7dc9f1eb81c8f56b16c7
H2Owater425 <h2o@dhmo.kr>
2024-08-17 12:19:47 +0900

    feat: Add whoareyou and goto command

--- source/index.ts
+++ source/index.ts
@@ -29,4 +29,102 @@ if(output !== null && input !== null && overlay !== null) {
 			throw new ShellError(['Usage: whoami']);
 		}
 	}, 'whoami', ['   Print the user name associated with the current effective user ID.']));
+
+	shell.register('whoareyou', new ShellCommand(function (this: Shell, _arguments: string[]): void {
+		if(_arguments['length'] === 1) {
+			this.puts('👋 Hello, stranger!',
+			'',
+			'I\'m H2Owater425, also known as KKM, which are my initials.',
+			'Currently, I\'m a senior in the Web Programming Department at Korea Digital Media High School. 👨‍🎓👨‍💻',
+			'My programming skills aren\'t the best, but I\'m a very passionate programmer!',
+			'Perhaps I could show you something as time goes on...',
+			'Anyway, have a nice day! ☀️',
+			'',
+			'Contacts 📫',
+			'- Discord: 물워터#7826',
+			'- Twitter: @H2Owater425',
+			'- Email: h2o@dhmo.kr, me@kangmin.kim')
+
+			return;
+		} else {
+			throw new ShellError(['Usage: whoareyou']);
+		}
+	}, 'whoareyou', ['   Display information about H2Owater425.']));
+
+	shell.register('goto', new ShellCommand(function (this: Shell, _arguments: string[]): void {
+		if(_arguments['length'] === 2) {
+			switch(_arguments[1].toLowerCase()) {
+				case 'github': {
+					window.open('https://github.com/H2Owater425', '_blank');
+
+					break;
+				}
+
+				case 'youtube': {
+					window.open('https://www.youtube.com/channel/UCaLPCyaHRP9f8SqYC9VckgA', '_blank');
+
+					break;
+				}
+
+				case 'twitch': {
+					window.open('https://www.twitch.tv/H2Owater425', '_blank');
+
+					break;
+				}
+
+				case 'chzzk': {
+					window.open('https://chzzk.naver.com/dd25515a05361dad0f0bcaf7f655530a', '_blank');
+
+					break;
+				}
+
+				case 'twitter': {
+					window.open('https://twitter.com/H2Owater425', '_blank');
+
+					break;
+				}
+
+				case 'x': {
+					window.open('https://x.com/H2Owater425', '_blank');
+
+					break;
+				}
+
+				case 'steam': {
+					window.open('https://steamcommunity.com/id/h2owater425', '_blank');
+
+					break;
+				}
+
+				case 'mochive': {
+					window.open('https://mochive.com/', '_blank');
+
+					break;
+				}
+
+				case 'forgor': {
+					window.open('https://forgor.sk/', '_blank');
+
+					break;
+				}
+
+				default: {
+					throw new ShellError(['sh: goto: no such website']);
+				}
+			}
+		} else {
+			throw new ShellError(['Usage: goto [website]']);
+		}
+	}, 'goto [website]', ['   Open a website related to H2Owater425.',
+	'',
+	'   Websites:',
+	'      github',
+	'      youtube',
+	'      twitch',
+	'      chzzk',
+	'      twitter',
+	'      x',
+	'      steam',
+	'      mochive',
+	'      forgor']));
 }
